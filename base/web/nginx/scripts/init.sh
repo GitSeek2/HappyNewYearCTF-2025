aa9defae823206a5fb99fc6331f12773e5ceff6d
QING <qlear@qq.com>
2025-02-26 00:31:23 +0800

    fix: 修复 init.sh 中错误的文件名

--- base/web/nginx/scripts/init.sh
+++ base/web/nginx/scripts/init.sh
@@ -3,7 +3,7 @@ if [ -z "$GZCTF_FLAG" ]; then
     GZCTF_FLAG="flag{seek_to_geek}"
 fi
 
-sed -i "s/flag{seek_to_geek}/$GZCTF_FLAG/g" /var/www/html/index.php
+sed -i "s/flag{seek_to_geek}/$GZCTF_FLAG/g" /var/www/html/index.html
 unset GZCTF_FLAG
 
 nginx -g 'daemon off;'
