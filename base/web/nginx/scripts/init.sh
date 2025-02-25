5857d2f7323804c23133008bb624c13483faee6a
QING <qlear@qq.com>
2025-02-25 23:16:29 +0800

    fix: some base nginx error

--- base/web/nginx/scripts/init.sh
+++ base/web/nginx/scripts/init.sh
@@ -2,9 +2,9 @@
 if [ -z "$GZCTF_FLAG" ]; then
     GZCTF_FLAG="flag{seek_to_geek}"
 fi
-# echo $GZCTF_FLAG >/flag
+
 sed -i "s/flag{seek_to_geek}/$GZCTF_FLAG/g" /var/www/html/index.php
 chmod 444 /flag
 unset GZCTF_FLAG
 
-rm -rf /init.sh
+nginx -g 'daemon off;'
